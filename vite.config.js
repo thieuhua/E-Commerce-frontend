43320015e36213ef9af57b31f4f74657e00f7bbd
thieuhua <141497957+thieuhua@users.noreply.github.com>
2026-06-15 11:17:37 +0700

    feat: update Vite configuration to load environment variables for server settings

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: { '@': path.resolve(__dirname, './src') },
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'https://q9kzb7l9-3000.asse.devtunnels.ms',
-        changeOrigin: true,
-      },
-      '/uploads': {
-        target: 'https://q9kzb7l9-3000.asse.devtunnels.ms/',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
       },
     },
-  },
+    server: {
+      port: Number(env.VITE_PORT),
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        },
+        '/uploads': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+  }
 })
